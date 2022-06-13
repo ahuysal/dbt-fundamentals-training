3ce17ca6cc6ebf5df55a6dcb724082819d8b1429
Ali Hilmi Uysal <uysalah@gmail.com>
2022-06-13 16:33:24 +0000

    huik

--- macros/clean_stale_models.sql
+++ macros/clean_stale_models.sql
@@ -1,19 +1,29 @@
-{% macro clean_stale_models(database = target.database, schema = target.schema) %}
+{% macro clean_stale_models(database=target.database, schema=target.schema, days=7, dry_run=True) %}
     
-    {% set drop_queries %}
+    {% set get_drop_commands_query %}
+        select
+            case 
+                when table_type = 'VIEW'
+                    then table_type
+                else 
+                    'TABLE'
+            end as drop_type, 
+            'DROP ' || drop_type || ' {{ database | upper }}.' || table_schema || '.' || table_name || ';'
+        from {{ database }}.information_schema.tables 
+        where table_schema = upper('{{ schema }}')
+        and last_altered <= current_date - {{ days }} 
+    {% endset %}
 
-        select case when table_type = 'VIEW' then table_type else 'TABLE' end as drop_type,
-        'DROP ' || drop_type || ' ' || table_name || ';' as q
-        from {{ database }}.INFORMATION_SCHEMA.TABLES
-        where TRUE
-        and table_schema = upper('{{ schema }}')
+    {{ log('\nGenerating cleanup queries...\n', info=True) }}
+    {% set drop_queries = run_query(get_drop_commands_query).columns[1].values() %}
 
-    {%endset%}
-
-    {% set queries = run_query(drop_queries).columns[1].values() %}
-
-    {% for q in queries%}
-            {{ log(q, info=True) }}
+    {% for query in drop_queries %}
+        {% if dry_run %}
+            {{ log(query, info=True) }}
+        {% else %}
+            {{ log('Dropping object with command: ' ~ query, info=True) }}
+            {% do run_query(query) %} 
+        {% endif %}       
     {% endfor %}
-
+    
 {% endmacro %}
